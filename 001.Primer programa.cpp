61fe8e16c1e3ea6b4cdef2d5389186b890eb07dc
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-03-25 13:41:34 -0500

    Agregando Hola Mundo c++

---
+++ 001.Primer programa.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+using namespace std;    //Para poder mostrar algo en pantalla
+int main()  //función tipo entero
+{
+    cout<<"Hola mundo :D"<<endl;     //cout imprime
+    return 0;
+}
