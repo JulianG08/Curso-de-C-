72ccacc6b7f31b38ac8e53adbbac62f5f5b44f92
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-03-25 13:42:10 -0500

    Agregando archivo Tipos de datos

---
+++ 002.Tipos de datos basicos.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int	entero = 15;	//tipo de dato entero
+	float flotante = 10.45;		//tipo de dato decimal
+	double mayor = 16.3456;		//flotante más largo
+	char letra = 'a';	//caracteres, debe ir en comillas simples
+	cout<<entero<<endl;
+	cout<<flotante<<endl;
+	cout<<mayor<<endl;
+	cout<<letra<<endl;
+	return 0;
+}
