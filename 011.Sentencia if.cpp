139ab2e480daab00a824a07752df7337db1835d4
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-03-27 17:06:57 -0500

    Agregando el archivo Sentencia if

---
+++ 011.Sentencia if.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int numero, dato = 5;
+	cout<<"Digite un numero: "; cin>>numero;
+	if (numero == dato)
+	{
+		cout<<"\nEl numero es 5"<<endl;
+	}
+	else
+	{
+		cout<<"\nEl numero es diferente de 5"<<endl;
+	}
+	return 0;
+}
