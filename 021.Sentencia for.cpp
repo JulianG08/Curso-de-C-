29912ee4afe0f4f5af7b59e6ee092a8935b8086b
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-04-09 17:39:35 -0500

    Agregando el archivo Sentencia for

---
+++ 021.Sentencia for.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	for (int i = 1; i <= 10; i++)
+	{
+		cout<<i<<endl;
+	}
+	cout<<" "<<endl;
+	for (int i = 10; i >= 1; i--)
+	{
+		cout<<i<<endl;
+	}
+	getch();
+	return 0;
+}
