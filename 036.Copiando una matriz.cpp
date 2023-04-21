78d793467eb72b0ab3c2e518151af7553cbed95a
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-04-21 17:13:47 -0500

    Agregando el archivo Copiando una matriz

---
+++ 036.Copiando una matriz.cpp
@@ -0,0 +1,27 @@
+/*Hacer una matriz de tipo entera de 2 * 2, llenarla de numeros y luego copiar todo su
+contenido hacia otra matriz*/
+#include <iostream>
+#include <conio.h>
+using namespace std;
+int main()
+{
+	int numeros[2][2] = {{1,2},{3,4}};
+	int matriz[2][2];
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < 2; j++)
+		{
+			matriz[i][j] = numeros[i][j];
+		}
+	}
+	for(int i = 0; i < 2; i++)
+	{
+		for(int j = 0; j < 2; j++)
+		{
+			cout<<matriz[i][j];
+		}
+		cout<<"\n";
+	}
+	getch();
+	return 0;
+}
