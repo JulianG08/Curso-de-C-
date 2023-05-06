94a214fa51c223aeb50f273127efa496a6ad94b9
Julián David Guarnizo Castrillón <juliandgc2018@hotmail.com>
2023-05-06 18:31:01 -0500

    Agregando el archivo Longitud de cadenas de caracteres

---
+++ 041.Longitud cadenas de caracteres.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <conio.h>
+#include <string.h>
+using namespace std;
+int main()
+{
+	char palabra[] = "Hola";
+	int longitud;
+	longitud = strlen(palabra);
+	cout<<"Numero de elementos de la cadena: "<<longitud<<endl;
+	getch();
+	return 0;
+}
